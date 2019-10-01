b3aa734377256a5d44ee775b6d989acee7020450
Michael_Mendiola22@outlook.com <Michael_Mendiola22@outlook.com>
2019-10-01 12:08:11 +0800

    Login

--- 846DentalClinicManagementSystem/Form1.Designer.cs
+++ 846DentalClinicManagementSystem/Form1.Designer.cs
@@ -28,20 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.picuser = new System.Windows.Forms.PictureBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picuser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.YellowGreen;
+            this.panel1.Controls.Add(this.btn_Login);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.picuser);
+            this.panel1.Controls.Add(this.txtUsername);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(47, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(420, 601);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(82, 436);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(270, 1);
+            this.panel3.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(82, 373);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(270, 1);
+            this.panel2.TabIndex = 14;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox1.BackgroundImage = global::_846DentalClinicManagementSystem.Properties.Resources.pass1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(82, 383);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.AllowDrop = true;
+            this.txtPassword.BackColor = System.Drawing.Color.YellowGreen;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Lucida Bright", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.HideSelection = false;
+            this.txtPassword.Location = new System.Drawing.Point(144, 393);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(208, 37);
+            this.txtPassword.TabIndex = 15;
+            this.txtPassword.TabStop = false;
+            this.txtPassword.Text = "Password";
+            // 
+            // picuser
+            // 
+            this.picuser.BackColor = System.Drawing.Color.Gray;
+            this.picuser.BackgroundImage = global::_846DentalClinicManagementSystem.Properties.Resources.user1;
+            this.picuser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picuser.Location = new System.Drawing.Point(82, 320);
+            this.picuser.Name = "picuser";
+            this.picuser.Size = new System.Drawing.Size(56, 47);
+            this.picuser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picuser.TabIndex = 13;
+            this.picuser.TabStop = false;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.AllowDrop = true;
+            this.txtUsername.BackColor = System.Drawing.Color.YellowGreen;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsername.Font = new System.Drawing.Font("Lucida Bright", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.HideSelection = false;
+            this.txtUsername.Location = new System.Drawing.Point(144, 330);
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(208, 37);
+            this.txtUsername.TabIndex = 9;
+            this.txtUsername.TabStop = false;
+            this.txtUsername.Text = "Username";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImage = global::_846DentalClinicManagementSystem.Properties.Resources.download;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(96, 46);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(227, 234);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.GreenYellow;
+            this.btn_Login.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.Location = new System.Drawing.Point(40, 454);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btn_Login.Size = new System.Drawing.Size(362, 38);
+            this.btn_Login.TabIndex = 18;
+            this.btn_Login.Text = "Log In";
+            this.btn_Login.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2970, 1023);
+            this.BackgroundImage = global::_846DentalClinicManagementSystem.Properties.Resources.a206masthead;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1354, 733);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picuser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox picuser;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btn_Login;
     }
 }
 
