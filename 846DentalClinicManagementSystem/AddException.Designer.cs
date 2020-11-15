3f578e0f916dd927d63b5d80d2206cb8b9a528f5
KiloEcho1122 <48122941+KiloEcho1122@users.noreply.github.com>
2020-11-15 08:54:15 +0800

    post deployment edit

--- 846DentalClinicManagementSystem/AddException.Designer.cs
+++ 846DentalClinicManagementSystem/AddException.Designer.cs
@@ -347,7 +347,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(505, 300);
+            this.ClientSize = new System.Drawing.Size(505, 429);
             this.Controls.Add(this.btn_cancelException);
             this.Controls.Add(this.btn_add);
             this.Controls.Add(this.bunifuCustomLabel1);
